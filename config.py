b1184bf800957245fdcffe0378a20d538b94e5e1
KoenigSwoy <89831897+KoenigSwoy@users.noreply.github.com>
2022-11-16 06:57:59 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    STANDARD_STATUS = "_"
+    STATUS = "{track} - {artist} - {album}"
+    VK_TOKEN = "3f44deefd555259bed08e85ba4d2da6bac1de27b80fc54b4ed27177b8874d50b329fee2fac28494e4c1d6"  # Access token for VKontakte account.
+    CLIENT_ID = "15d5ab582dfb4756b9354ca71efc0bd2"  # Client ID for spotify application.
+    CLIENT_SECRET = "510335f39b054c38af5043c74545fbb"  # Client secret for spotify application.
+    REDIRECT_URI = "http://localhost:8888"  # Redirect uri for spotify application.
+    USERNAME = "w48mrizts7vrh5ozpuwftfa1x"  # Your name in spotify.
+    SCOPE = "user-read-playback-state user-library-read"  # Spotify scopes.
